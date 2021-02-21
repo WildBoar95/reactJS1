b7b266e676ba31782f38754350250d5d6f8ea4e2
WildBoar95 <dukuukaban4ik@gmail.com>
2021-02-22 01:30:48 +0300

    header

--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,9 @@
 import React from 'react';
 const Header = () => {
   return (<div>
-    <a href='#s'>Home</a>
-    <a href='#s'>News Feed</a>
-    <a href='#s'>Masseges</a>
+    <a href='#s'>Home</a>-
+    <a href='#s'>News Feed</a>-
+    <a href='#s'>Masseges</a>-
   </div>);
 }
 
