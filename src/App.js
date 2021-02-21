ebf08e6160beba36b9c224bab7256fd615d15bff
WildBoar95 <dukuukaban4ik@gmail.com>
2021-02-22 00:23:28 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Header from './Header.js';
+import Techologies from './Technologies.js';
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <Techologies />
+    </div>);
+}
+
+export default App;
